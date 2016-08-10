857b1f6196b7c131953fb3f89fc8176222693de0
Jake Teton-Landis <jake.tl@airbnb.com>
2016-08-10 11:13:54 -0700

    remove TODO that was done

--- lib/appear/constants.rb
+++ lib/appear/constants.rb
@@ -7,9 +7,6 @@ module Appear
   # root error for our library; all other errors inherit from this one.
   class Error < StandardError; end
 
-  # we also put common constants in here because it's a good spot.
-  # Should we rename this file to 'appear/constants' ?
-
   # the root of the Appear project directory
   MODULE_DIR = Pathname.new(__FILE__).realpath.join('../../..')
 
