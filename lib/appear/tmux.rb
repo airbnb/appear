e1f0b2623ace42c86c568e7356718cccae9078c9
Jake Teton-Landis <jake.tl@airbnb.com>
2016-08-24 15:10:40 -0700

    Tmux: implement object model

--- lib/appear/tmux.rb
+++ lib/appear/tmux.rb
@@ -9,11 +9,37 @@ module Appear
   #
   # see the man page for tmux if you are curious about what clients, windows,
   # panes, and sessions are in Tmux world.
+  #
+  # @todo move value object parsing and format string into value class definition
+  # @todo all create-foo methods should return a value class of the thing
+  #   created, so we don't have to re-select it.
+  # @todo refactor into smaller files?
   class Tmux < Service
     delegate :run, :runner
 
+    class TmuxValue < ::Appear::Util::ValueClass
+      # @return [Tmux] the tmux service that created this pane
+      attr_reader :tmux
+
+      def self.attr_map(map)
+        @tmux_attr_map = map
+      end
+
+      def self.format_string
+        hsh = {}
+        @tmux_attr_map.each do |k, v|
+          if v.is_a? Array
+            hsh[h] = v.first
+          else
+            hsh[h] = v
+          end
+        end
+        hsh
+      end
+    end
+
     # A tmux pane.
-    class Pane < ::Appear::Util::ValueClass
+    class Pane < TmuxValue
       # @return [Fixnum] pid of the process running in the pane
       attr_reader :pid
 
@@ -31,10 +57,72 @@ module Appear
 
       # @return [String] command running in this pane
       attr_reader :command_name
+
+      # @return [String] pane current path
+      attr_reader :current_path
+
+      # String suitable for use as the "target" specifier for a Tmux command
+      #
+      # @return [String]
+      def target
+        "#{session}:#{window}.#{pane}"
+      end
+
+      def split(opts = {})
+        tmux.split_window(opts.merge(:t => target))
+      end
+    end
+
+    # A tmux session.
+    # Has many windows.
+    class Session < TmuxValue
+      # @return [String] session name
+      attr_reader :session
+
+      def target
+        session
+      end
+
+      def windows
+        tmux.windows.select { |w| w.session == session }
+      end
+
+      def clients
+        tmux.clients.select { |c| c.session == session }
+      end
+
+      def new_window(opts = {})
+        win = windows.last.window || -1
+        tmux.new_window(opts.merge(:t => "#{target}:#{win + 1}"))
+      end
+    end
+
+    # A tmux window.
+    # Has many panes.
+    class Window < TmuxValue
+      # @return [String] session name
+      attr_reader :session
+
+      # @return [Fixnum] window index
+      attr_reader :window
+
+      # @return [String] window id
+      attr_reader :id
+
+      # @return [Boolean] is the window active?
+      attr_reader :active?, :active
+
+      def panes
+        tmux.panes.select { |p| p.session == session && p.window == window }
+      end
+
+      def target
+        "#{session}:#{window}"
+      end
     end
 
     # A tmux client.
-    class Client < ::Appear::Util::ValueClass
+    class Client < TmuxValue
       # @return [String] path to the TTY device of this client
       attr_reader :tty
 
@@ -43,6 +131,9 @@ module Appear
 
       # @return [String] session name
       attr_reader :session
+
+      # @return [Tmux] the tmux service that created this pane
+      attr_reader :tmux
     end
 
     # List all the tmux clients on the system
@@ -53,7 +144,7 @@ module Appear
           :tty => :client_tty,
           :term => :client_termname,
           :session => :client_session
-      ))).map { |c| Client.new(c) }
+      ))).map { |c| Client.new(c.merge(:tmux => self)) }
     end
 
     # List all the tmux panes on the system
@@ -66,12 +157,53 @@ module Appear
         :window => :window_index,
         :pane => :pane_index,
         :command_name => :pane_current_command,
+        :current_path => :pane_current_path,
         :active => :pane_active
       ))).map do |pane|
         Pane.new(pane.merge(
           :window => pane[:window].to_i,
+          :pane => pane[:pane].to_i,
           :pid => pane[:pid].to_i,
-          :active => pane[:active].to_i != 0
+          :active => pane[:active].to_i != 0,
+          :tmux => self
+        ))
+      end
+    end
+
+    # List all the tmux sessions on the system
+    #
+    # @return [Array<Session>]
+    def sessions
+      ipc(command('list-sessions').flags(:F => format_string(
+        :session => :session_name,
+        :id => :session_id,
+        :attached => :session_attached,
+        :width => :session_width,
+        :height => :session_height,
+      ))).map do |s|
+        Session.new(s.merge(
+          :tmux => self,
+          :attached => s[:attached].to_i,
+          :width => s[:width].to_i,
+          :height => s[:height].to_i,
+        ))
+      end
+    end
+
+    # List all the tmux windows in any session on the system
+    #
+    # @return [Array<Window>]
+    def windows
+      ipc(command('list-windows').flags(:a => true, :F => format_string(
+        :session => :session_name,
+        :window => :window_index,
+        :id => :window_id,
+        :active => :window_active,
+      ))).map do |w|
+        Window.new(w.merge(
+          :tmux => self,
+          :window => w[:window].to_i,
+          :active => w[:window].to_i != 0,
         ))
       end
     end
@@ -82,6 +214,31 @@ module Appear
     def reveal_pane(pane)
       ipc(command('select-pane').flags(:t => "#{pane.session}:#{pane.window}.#{pane.pane}"))
       ipc(command('select-window').flags(:t => "#{pane.session}:#{pane.window}"))
+    end
+
+    def new_window(opts = {})
+      run(command('new-window').flags(opts).to_a)
+    end
+
+    def split_window(opts = {})
+      run(command('split-window').flags(opts).to_a)
+    end
+
+    def new_session(opts = {})
+      s = ipc(command('new-session').flags(opts.merge(:F => format_string(
+        :session => :session_name,
+        :id => :session_id,
+        :attached => :session_attached,
+        :width => :session_width,
+        :height => :session_height,
+      )))).first
+
+      Session.new(s.merge(
+        :tmux => self,
+        :attached => s[:attached].to_i,
+        :width => s[:width].to_i,
+        :height => s[:height].to_i,
+      ))
     end
 
     private
